f9c12b4a09ea51ab9e66f0cb12e54e9b9c8f3dab
liuming.cpp <liuming.cpp@gmail.com>
2017-02-13 17:38:28 +0800

    x64 tls supported

--- tls/tls.cpp
+++ tls/tls.cpp
@@ -4,53 +4,56 @@
 #include "stdafx.h"
 #include <windows.h>
 
-//下面这行告诉链接器在PE文件中要创建TLS目录   
-#pragma comment(linker, "/INCLUDE:__tls_used")   
+//下面这行告诉链接器在PE文件中要创建TLS目录
+#ifdef _WIN64
+#pragma comment (linker, "/INCLUDE:_tls_used") //引用tlssup.cpp中的_tls_used
+#pragma comment (linker, "/INCLUDE:p_tls_callback") //引用下，防止被编译器优化掉（或者去掉/GL优化开关）
+#else
+#pragma comment (linker, "/INCLUDE:__tls_used")
+#pragma comment (linker, "/INCLUDE:_p_tls_callback") //引用下，防止被编译器优化掉（或者去掉/GL优化开关）
+#endif
+
 /*这是PIMAGE_TLS_CALLBACK()函数的原型，其中第一个和第三个参数保留，第二个参数决定函数在那种情况下*/
 /*一共有四个选项DLL_PROCESS_ATTACH、DLL_THREAD_ATTACH、DLL_THREAD_DETACH和DLL_PROCESS_DETACH。详见微软发布的《Microsoft Portable Executable and Common Object File Format Specification v8》*/
 void NTAPI exe_tls_callback1(PVOID h, DWORD reason, PVOID pv) {
     if (reason == DLL_PROCESS_ATTACH) {
-        printf("hi,this is %s, DLL_PROCESS_ATTACH, tid(%d)\n", __FUNCTION__, GetCurrentThreadId());
+        OutputDebugStringA("hi,this is exe_tls_callback1, DLL_PROCESS_ATTACH\n"); //crt可能还没初始化，不能调用printf
     } else if (reason == DLL_THREAD_ATTACH) {
-        printf("hi,this is %s, DLL_THREAD_ATTACH, tid(%d)\n", __FUNCTION__, GetCurrentThreadId());;
+        OutputDebugStringA("hi,this is exe_tls_callback1, DLL_THREAD_ATTACH\n");
     }
     return;
 }
 
 void NTAPI exe_tls_callback2(PVOID h, DWORD reason, PVOID pv) {
     if (reason == DLL_PROCESS_ATTACH) {
-        printf("hi,this is %s, DLL_PROCESS_ATTACH, tid(%d)\n", __FUNCTION__, GetCurrentThreadId());
+        OutputDebugStringA("hi,this is exe_tls_callback2, DLL_PROCESS_ATTACH\n");
     } else if (reason == DLL_THREAD_ATTACH) {
-        printf("hi,this is %s, DLL_THREAD_ATTACH, tid(%d)\n", __FUNCTION__, GetCurrentThreadId());;
+        OutputDebugStringA("hi,this is exe_tls_callback2, DLL_THREAD_ATTACH\n");
     }
-    return;;
+    return;
 }
 
 /*
-这里使用crt库的tls callback机制，详见tlsup.cpp($(VSInstallDir)VC\crt\src\vcruntime\tlsup.cpp)
+这里使用crt库的tls callback机制，详见tlssup.cpp($(VSInstallDir)VC\crt\src\vcruntime\tlssup.cpp)
 tlsup.cpp中定义了：
 _CRTALLOC(".CRT$XLA") PIMAGE_TLS_CALLBACK __xl_a = 0;
 _CRTALLOC(".CRT$XLZ") PIMAGE_TLS_CALLBACK __xl_z = 0;
 
 由于编译器机制，".CRT$XLB"~".CRT$XLY"节的地址将按顺序分布在".CRT$XLA"和".CRT$XLZ"之间，形成一段连续的地址空间，可以当作一个数组
 */
-#pragma data_seg(".CRT$XLB")
-PIMAGE_TLS_CALLBACK p_tls_callback[] = { exe_tls_callback1, exe_tls_callback2, 0 };
-#pragma data_seg()
+#pragma const_seg(".CRT$XLB")
+EXTERN_C const PIMAGE_TLS_CALLBACK p_tls_callback[] = { exe_tls_callback1, exe_tls_callback2, 0 };
+#pragma const_seg()
 
 void test_thread();
 
 int main()
 {
-    //引用下，防止被编译器优化掉（或者去掉/GL优化开关）
-    // vs 2015会优化，vs 2008不会
-    printf("%d\n", reinterpret_cast<int>(p_tls_callback)); 
-
-    printf("hi,this is %s, tid(%d)\n", __FUNCTION__, GetCurrentThreadId());
-    HMODULE tlsdll = LoadLibraryW(L"tlsdll.dll");
+    OutputDebugStringA("hi,this is main\n");
 
     test_thread();
-    
+
+    //HMODULE tlsdll = LoadLibraryW(L"tlsdll.dll");
     //typedef void(*test_t)();
     //test_t test = (test_t)GetProcAddress(tlsdll, "test_thread");
     //test();
--- tlsdll/tlsdll.cpp
+++ tlsdll/tlsdll.cpp
@@ -4,47 +4,54 @@
 #include "stdafx.h"
 
 //下面这行告诉链接器在PE文件中要创建TLS目录   
-#pragma comment(linker, "/INCLUDE:__tls_used")   
+#ifdef _WIN64
+#pragma comment (linker, "/INCLUDE:_tls_used") //引用tlssup.cpp中的_tls_used
+#pragma comment (linker, "/INCLUDE:p_tls_callback") //引用下，防止被编译器优化掉（或者去掉/GL优化开关）
+#else
+#pragma comment (linker, "/INCLUDE:__tls_used")
+#pragma comment (linker, "/INCLUDE:_p_tls_callback") //引用下，防止被编译器优化掉（或者去掉/GL优化开关）
+#endif
+
 /*这是PIMAGE_TLS_CALLBACK()函数的原型，其中第一个和第三个参数保留，第二个参数决定函数在那种情况下*/
 /*一共有四个选项DLL_PROCESS_ATTACH、DLL_THREAD_ATTACH、DLL_THREAD_DETACH和DLL_PROCESS_DETACH。详见微软发布的《Microsoft Portable Executable and Common Object File Format Specification v8》*/
 void NTAPI dll_tls_callback1(PVOID h, DWORD reason, PVOID pv) {
     if (reason == DLL_PROCESS_ATTACH) {
-        printf("hi,this is %s, DLL_PROCESS_ATTACH, tid(%d)\n", __FUNCTION__, GetCurrentThreadId());
+        OutputDebugStringA("hi,this is dll_tls_callback1, DLL_PROCESS_ATTACH\n"); //crt可能还没初始化，不能调用printf
     } else if (reason == DLL_THREAD_ATTACH) {
-        printf("hi,this is %s, DLL_THREAD_ATTACH, tid(%d)\n", __FUNCTION__, GetCurrentThreadId());;
+        OutputDebugStringA("hi,this is dll_tls_callback1, DLL_THREAD_ATTACH\n");
     }
     return;
 }
 
 void NTAPI dll_tls_callback2(PVOID h, DWORD reason, PVOID pv) {
     if (reason == DLL_PROCESS_ATTACH) {
-        printf("hi,this is %s, DLL_PROCESS_ATTACH, tid(%d)\n", __FUNCTION__, GetCurrentThreadId());
+        OutputDebugStringA("hi,this is dll_tls_callback2, DLL_PROCESS_ATTACH\n");
     } else if (reason == DLL_THREAD_ATTACH) {
-        printf("hi,this is %s, DLL_THREAD_ATTACH, tid(%d)\n", __FUNCTION__, GetCurrentThreadId());;
+        OutputDebugStringA("hi,this is dll_tls_callback2, DLL_THREAD_ATTACH\n");
     }
-    return;;
+    return;
 }
 
 /*
-这里使用crt库的tls callback机制，详见tlsup.cpp($(VSInstallDir)VC\crt\src\vcruntime\tlsup.cpp)
+这里使用crt库的tls callback机制，详见tlssup.cpp($(VSInstallDir)VC\crt\src\vcruntime\tlssup.cpp)
 tlsup.cpp中定义了：
 _CRTALLOC(".CRT$XLA") PIMAGE_TLS_CALLBACK __xl_a = 0;
 _CRTALLOC(".CRT$XLZ") PIMAGE_TLS_CALLBACK __xl_z = 0;
 
 由于编译器机制，".CRT$XLB"~".CRT$XLY"节的地址将按顺序分布在".CRT$XLA"和".CRT$XLZ"之间，形成一段连续的地址空间，可以当作一个数组
 */
-#pragma data_seg(".CRT$XLB")
-PIMAGE_TLS_CALLBACK p_tls_callback[] = { dll_tls_callback1, dll_tls_callback2, 0 };
-#pragma data_seg()
+#pragma const_seg(".CRT$XLB")
+EXTERN_C const PIMAGE_TLS_CALLBACK p_tls_callback[] = { dll_tls_callback1, dll_tls_callback2, 0 };
+#pragma const_seg()
 
 DWORD WINAPI ThreadProc(
     _In_ LPVOID lpParameter
 ) {
-    printf("hi,this is %s, tid(%d)\n", __FUNCTION__, GetCurrentThreadId());
+    OutputDebugStringA("hi,this is ThreadProc\n");
     return 0;
 }
 
 void test_thread() {
-    HANDLE thread = CreateThread(NULL, 0, ThreadProc, p_tls_callback, 0, NULL);
+    HANDLE thread = CreateThread(NULL, 0, ThreadProc, NULL, 0, NULL);
     WaitForSingleObject(thread, INFINITE);
 }
